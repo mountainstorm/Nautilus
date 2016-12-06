c43d2dc90dabfdb437af850726da63f4a3aa4ec2
@Lord_Coops <cooper@coopers-imac.home>
2016-12-06 18:57:45 +0000

    fixed build script

--- build.netatalk.sh
+++ build.netatalk.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 # coding: utf-8
 
+
 NETATALK_URL=http://www003.upp.so-net.ne.jp/hat/files/netatalk-3.1.10-0.1.1.fc26.src.rpm
 
 
 # reference: http://netatalk.sourceforge.net/wiki/index.php/Netatalk_3.1.10_SRPM_for_Fedora_and_CentOS
 # install pre-reqs
-PWD=$(pwd)
-yum -y install rpm-build gcc make yum-utils >> ${PWD}/build.log
+CWD=$(pwd)
+yum -y install rpm-build gcc make yum-utils >> ${CWD}/build.log
 
 # download src rpm and extract it
 pushd ./
 cd ~/
 curl -o netatalk.rpm $NETATALK_URL
-rpm -ivh netatalk.rpm >> ${PWD}/build.log
+rpm -ivh netatalk.rpm >> ${CWD}/build.log
 
 # go into the specs and build the binary rpm
 cd ~/rpmbuild/SPECS/
-yum-builddep netatalk.spec >> ${PWD}/build.log
-rpmbuild -bb netatalk.spec >> ${PWD}/build.log
+yum-builddep -y netatalk.spec >> ${CWD}/build.log
+rpmbuild -bb netatalk.spec >> ${CWD}/build.log
 
 # upload the binary rpm back to local server
 popd
